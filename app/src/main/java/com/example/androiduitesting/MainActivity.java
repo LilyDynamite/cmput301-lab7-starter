27fc6230205322934b42d18cf3f4ba15fa13bedc
LilyDynamite <99293535+LilyDynamite@users.noreply.github.com>
2025-10-21 21:25:14 -0600

    lab participation exercise

--- app/src/main/java/com/example/androiduitesting/MainActivity.java
+++ app/src/main/java/com/example/androiduitesting/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.androiduitesting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout nameField;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    static String nameToDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,5 +68,25 @@ public class MainActivity extends AppCompatActivity {
                 cityAdapter.clear();
             }
         });
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //item was clicked
+                Log.println(Log.WARN, "LILY", "item clicked");
+                nameToDisplay = cityAdapter.getItem(i); //name we will be able to give to ShowActivity
+
+                Intent showActivityView = new Intent(MainActivity.this, ShowActivity.class);
+
+                startActivity(showActivityView);
+
+            }
+        });
+
+    }
+
+    //Returns the name to display on the ShowActivity screen
+    public static String getTextToDisplay()
+    {
+        return nameToDisplay;
     }
 }
---
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -0,0 +1,47 @@
+package com.example.androiduitesting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ShowActivity extends AppCompatActivity {
+
+    TextView displayText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_show2);
+        /*
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+         */
+        displayText = findViewById(R.id.display_city_text);
+        displayText.setText(MainActivity.getTextToDisplay());
+
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent mainActivityView = new Intent(ShowActivity.this, MainActivity.class);
+                startActivity(mainActivityView);
+
+            }
+        });
+
+    }
+
+
+
+}
